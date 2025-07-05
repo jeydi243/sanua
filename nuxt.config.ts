b466beff8c7abd7151d5d2beb96fa4d3fa6df478
KADIONGO ILUNGA Epaphrodite <kadiongo.ilunga@gecamines.cd>
2025-07-05 22:40:59 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    'shadcn-nuxt',
+  ],
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
+})
---
+++ pages/cashier/deposit.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+            Le monde est beau
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
